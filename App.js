02bdd08c909b80c5ee96284cb12feb8ed583a371
CherryA <cherryanncagoco@gmail.com>
2025-09-25 13:11:50 +0800

    main

---
+++ App.js
@@ -0,0 +1,13 @@
+import Comment from './Comment';
+import ChatScreen from './ChatScreen';
+import React from "react";
+
+
+export default function App() {
+  return ( <>
+<ChatScreen />
+<Comment />
+
+</>
+)
+}
